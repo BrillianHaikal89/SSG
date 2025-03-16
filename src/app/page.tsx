41fbb4598a843b6283524d95552007de85340e87
BrillianHaikal89 <brillianulbi@gmail.com>
2025-03-16 15:37:48 +0700

    menambahkan halaman about us.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Navbar from '../components/navbar';
