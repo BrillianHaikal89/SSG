cfb2c0f4dd0d43cc1e3b926adff7974e5718f3b3
Ilhamarip arip <ilhamariforpin007tea@gmail.com>
2025-04-30 03:08:44 +0000

    gaksihe

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export', // 👈 ini kunci agar bisa jadi static site
-  reactStrictMode: true,
+  // output: 'export', // 👈 ini kunci agar bisa jadi static site
+  // reactStrictMode: true,
   // Tambahkan config lain jika perlu
 }
 
