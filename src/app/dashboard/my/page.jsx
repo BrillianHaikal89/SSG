64ac95e08d934f2d108ba39f9a38f66c4f669df6
ekal <macbookpro2017@Macbooks-MacBook-Pro.local>
2025-05-02 11:29:53 +0700

    menambah fitur MY

--- src/app/dashboard/my/page.jsx
+++ src/app/dashboard/my/page.jsx
@@ -350,7 +350,7 @@ export default function MutabahYaumiyahPage() {
                 className="form-checkbox h-4 w-4 sm:h-5 sm:w-5 text-red-600 rounded focus:ring-red-500" 
               />
               <span className="ml-2 text-xs sm:text-sm text-gray-700">
-                Sedang berhalangan (haid/menstruasi) dan tidak dapat melaksanakan sholat
+                Sedang berhalangan (haid/menstruasi) dan tidak dapat melaksanakan sholat wajib dan sunnah
               </span>
             </label>
           </div>
