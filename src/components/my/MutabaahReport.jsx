eb0ffd9dd3c31144b846b8767d508109eeb197a3
Ilhamarip <155143109+ilhamganteng321@users.noreply.github.com>
2025-05-04 11:40:57 +0700

    Update MutabaahReport.jsx

--- src/components/my/MutabaahReport.jsx
+++ src/components/my/MutabaahReport.jsx
@@ -55,7 +55,7 @@ const MutabaahReport = ({ user, onClose }) => {
       
       const API_URL = process.env.NEXT_PUBLIC_API_URL;
       const response = await fetch(`${API_URL}/users/get-ibadah-month?user_id=${user.userId}&month=${month}&year=${year}`, {
-        method: 'POST',
+        method: 'GET',
         headers: {
           'Content-Type': 'application/json',
           }
