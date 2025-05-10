d9aabf50456b5d2a764e761a72717127f762fd51
ekal <macbookpro2017@Macbooks-MacBook-Pro.local>
2025-05-10 21:04:37 +0700

    Baru lagi nih hamszzkuy

--- src/app/dashboard/my/page.jsx
+++ src/app/dashboard/my/page.jsx
@@ -367,8 +367,7 @@ export default function MutabaahYaumiyahPage() {
             sholat_tahajud: false,
             sholat_dhuha: 0,
             sholat_rawatib: 0,
-            sholat_sunnah_lainnya: 0,
-            shaum_sunnah: false
+            sholat_sunnah_lainnya: 0
           } : {})
         };
         
