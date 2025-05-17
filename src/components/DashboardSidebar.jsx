1a6205e6f575ab14a54a89fbb542f4a06b0f089a
ekal <macbookpro2017@Macbooks-MacBook-Pro.local>
2025-05-17 13:02:12 +0700

    aku nambah ini hamsz memisahkan rolerole

--- src/components/DashboardSidebar.jsx
+++ src/components/DashboardSidebar.jsx
@@ -36,7 +36,7 @@ const DashboardSidebar = ({
                 navigateToProfile();
                 closeSidebar && closeSidebar();
             },
-            roles: ['0a', '0b', '1a'] 
+            roles: ['0a', '0b', '1a']
         },
         {
             id: 'my',
@@ -50,7 +50,7 @@ const DashboardSidebar = ({
                 navigateToMY();
                 closeSidebar && closeSidebar();
             },
-            roles: ['all'] 
+            roles: ['all']
         },
         {
             id: 'quran',
@@ -64,7 +64,7 @@ const DashboardSidebar = ({
                 navigateToAlQuran();
                 closeSidebar && closeSidebar();
             },
-            roles: ['all'] 
+            roles: ['all']
         },
         {
             id: 'presensi',
@@ -78,13 +78,14 @@ const DashboardSidebar = ({
                 navigateToPresensi();
                 closeSidebar && closeSidebar();
             },
-            roles: ['1a'] 
+            roles: ['1a']
         },
         {
             id: 'lihat-presensi',
             icon: (
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                 </svg>
             ),
             label: "Lihat Presensi",
@@ -92,7 +93,7 @@ const DashboardSidebar = ({
                 navigateToLihatPresensi();
                 closeSidebar && closeSidebar();
             },
-            roles: ['3', '4'] 
+            roles: ['3', '4']
         },
         {
             id: 'tugas',
@@ -106,7 +107,7 @@ const DashboardSidebar = ({
                 navigateToTugas();
                 closeSidebar && closeSidebar();
             },
-            roles: ['1a', '2c', '3'] 
+            roles: ['1a', '2c', '3']
         },
         {
             id: 'scanqr',
@@ -120,7 +121,7 @@ const DashboardSidebar = ({
                 navigateToScan();
                 closeSidebar && closeSidebar();
             },
-            roles: ['2c'] 
+            roles: ['2c']
         },
         {
             id: 'kegiatan',
@@ -134,7 +135,7 @@ const DashboardSidebar = ({
                 navigateToKelolaKegiatan();
                 closeSidebar && closeSidebar();
             },
-            roles: ['3'] 
+            roles: ['3']
         }
     ];
 
