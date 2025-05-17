9a9f3b2c0dcd0b035bbdb1fcf13079f010ca8525
ekal <macbookpro2017@Macbooks-MacBook-Pro.local>
2025-05-17 12:41:58 +0700

    aku nambah ini hamsz memisahkan rolerole

--- src/components/DashboardContent.jsx
+++ src/components/DashboardContent.jsx
@@ -373,8 +373,8 @@ const DashboardContent = ({
         </div>
       </div>
 
-      {/* Pengumuman Section - Show for all roles except 0a */}
-      {role !== '0a' && (
+      {/* Pengumuman Section - Show for all roles except 0a & 0b */}
+      {role === '0a' || role === '0b' && (
         <section className="bg-orange-300 rounded-lg shadow-sm mb-4 p-4">
           <h3 className="font-bold text-sm mb-2">Pengumuman</h3>
           
